9625e94f1cf7621dc3e4aa216e2e2f2e92bbc5f5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-07 14:29:18 -0800

    added startup script and killing only if codecombat runs

--- bin/stop_server.sh
+++ bin/stop_server.sh
@@ -4,8 +4,14 @@ cd "`dirname \"$0\"`"
 
 if [ -f codecombat.pid ]
 then
-  sudo kill -9 `cat codecombat.pid`
-  echo "killed codecombat server with pid "`cat codecombat.pid`
+  if wget -O - http://locahost:3000 >>/dev/null
+  then
+    echo "codecombat running on port 3000"
+    sudo kill -9 `cat codecombat.pid`
+    echo "killed codecombat server with pid "`cat codecombat.pid`
+  else
+    echo "codecombat is not running"
+  fi
   rm codecombat.pid
 fi
 
